6276792020dac1c3f401f4ecf499c0bfa227f1ab
Pegah Rashidnia <PRashidnia@gmail.com>
2016-08-04 12:17:38 -0400

    Checks for special characters, added more documentation, removed getAnswers function
    didn't need getAnswers; toGIFT works fine

--- html-to-GIFT.py
+++ html-to-GIFT.py
@@ -1,19 +1,36 @@
 from bs4 import BeautifulSoup
 import os
-#Opens HTML file
+
 def open_file(html_file):
+    """
+    opens HTML file using Beautiful Soup
+
+    :param html_file: input HTML file
+    :return: parsed HTML
+    """
     soup = BeautifulSoup(open(html_file), "lxml")
     return soup
 
 def getAnswers(html_file, answer):
+    """
+
+    :param html_file:
+    :param answer:
+    :return:
+    """
     answers = []
     for ans in answer[1]:
         answers.append(ans)
     #Inserts all questions into a list
 
+    spec_char = ['=', '<', '>', '#', '&']
     question = []
     for li in html_file.findAll('li'):
-        question.append(li.get_text().strip())
+        quest = li.get_text().strip()
+        for ch in spec_char:
+            if ch in quest:
+                quest = quest.replace(ch, "\\"+ch)
+        question.append(quest)
 
     questAns = []
     listQA = []
@@ -32,17 +49,21 @@ def toGIFT(html_file, outFile, quizName):
         answers.append(ans)
     getAnswers(html_file, heading)
 
-    #outFile.write("::" + title + "::")
+    spec_char = ['=', '#', '&']
     index = 0
     for li in html_file.findAll('li'):
         name = li.get_text().strip()
         outFile.write( "::" + quizName + "::" + name.encode('utf-8'))
         outFile.write("{\n")
         for ans in li.findAll('asp:listitem', {'text': True, 'value': True}):
+            quest = ans['text']
+            for ch in spec_char:
+                if ch in quest:
+                    quest = quest.replace(ch, "\\" + ch)
             if ans['value'] == answers[index]:
-                outFile.write("=" + ans['text'].encode('utf-8') + "\n")
+                outFile.write("=" + quest.encode('utf-8') + "\n")
             else:
-                outFile.write("~" + ans['text'].encode('utf-8') + "\n")
+                outFile.write("~" + quest.encode('utf-8') + "\n")
         outFile.write("} \n\n")
         index = index + 1
 
@@ -57,11 +78,10 @@ for file in os.listdir("C:\Users\Pegah\OneDrive\Programming\Work\Quizzes"):
         quizName = file.split(".")
         quizN = quizName[0]
         quizN = quizN.encode('utf-8')
-        #quizName = quizName[1].split(".")
         toGIFT(htmlFile, moodleFile, quizName[0])
         moodleFile.close()
         count += 1
 
 print(count)
-#file = "Quizzes/Lecture2Quiz3.html"
+
 
